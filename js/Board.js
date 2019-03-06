2300f622358e6ae867675f8b0e0ce2886a8924b5
dominik-rodzim <46093484+dominik-rodzim@users.noreply.github.com>
2019-03-06 20:33:09 +0100

    Update Board.js

--- js/Board.js
+++ js/Board.js
@@ -1,10 +1,10 @@
 var board = {
-    name: 'Tablica Kanban',
-    addColumn: function(column) {
-      this.element.appendChild(column.element);
-      initSortable(column.id);
-    },
-    element: document.querySelector('#board .column-container')
+  name: 'Tablica Kanban',
+  addColumn: function(column) {
+    this.element.appendChild(column.element);
+    initSortable(column.id);
+  };
+  element: document.querySelector('#board .column-container')
 };
 
 document.querySelector('#board .create-column').addEventListener('click', function() {
@@ -14,23 +14,23 @@ document.querySelector('#board .create-column').addEventListener('click', functi
   data.append('name', name);
 
   fetch(baseUrl + '/column', {
-      method: 'POST',
-      headers: myHeaders,
-      body: data,
-    })
-    .then(function(resp) {
-      return resp.json();
-    })
-    .then(function(resp) {
-      var column = new Column(resp.id, name);
-      board.addColumn(column);
-    });
+    method: 'POST',
+    headers: myHeaders,
+    body: data,
+  })
+  .then(function(resp) {
+    return resp.json();
+  })
+  .then(function(resp) {
+    var column = new Column(resp.id, name);
+    board.addColumn(column);
+  });
 });
 	
 function initSortable(id) {
-  	var el = document.getElementById(id);
-  	var sortable = Sortable.create(el, {
-    	group: 'kanban',
-    	sort: true
-  	});
+  var el = document.getElementById(id);
+  var sortable = Sortable.create(el, {
+    group: 'kanban',
+    sort: true
+  });
 }
